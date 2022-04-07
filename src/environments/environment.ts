b25764b7d767e0d8f70dbedb60ad56f36ad055de
Nikita Dachkovskii <noxik7777@gmail.com>
2022-04-07 16:51:11 +0300

    Change styles and clear unused values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,11 @@
-export const environment = {
+export const environment: Environment = {
   production: false,
   apiUrl: 'https://www.superheroapi.com/api.php/',
   apiToken: '2246724375469588/'
 };
+
+export interface Environment {
+  production: boolean,
+  apiUrl: string,
+  apiToken: string
+}
