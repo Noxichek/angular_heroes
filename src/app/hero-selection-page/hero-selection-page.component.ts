a250bcf05595313f0eeb9d00ce4dcf42c069f19c
Nikita Dachkovskii <noxik7777@gmail.com>
2022-04-13 18:36:53 +0300

    Change types and create onDestroy

--- src/app/hero-selection-page/hero-selection-page.component.ts
+++ src/app/hero-selection-page/hero-selection-page.component.ts
@@ -1,42 +1,51 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FetchService} from "../global/services/fetch.service";
 import {Letter} from "../models/alphabet.model";
 import {StoreService} from "../global/services/store.service";
-import {UserStateKeys} from "../shared/interfaces";
+import {Hero, UserStateKeys} from "../shared/interfaces";
+import {Subscription} from "rxjs";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-hero-selection-page',
   templateUrl: './hero-selection-page.component.html',
   styleUrls: ['./hero-selection-page.component.scss']
 })
-export class HeroSelectionPageComponent {
+export class HeroSelectionPageComponent implements OnInit, OnDestroy {
 
   searchValue: string = '';
-  stats: Array<any> = [];
-  heroes: Array<any> = [];
+  stats = [];
+  heroes: Array<Hero> = [];
+  randomHeroSub: Subscription;
+  heroByNameSub: Subscription;
+  searchForm: FormGroup;
+  inputSearch: any;
 
   constructor(private fetchService: FetchService, public storeService: StoreService) {
-    fetchService.getRandomHero().subscribe(response => {
+  }
+
+  ngOnInit(): void {
+    this.randomHeroSub = this.fetchService.getRandomHero().subscribe(response => {
       this.heroes = [response]
     })
   }
 
   search() {
     const value = this.searchValue.toString().toLowerCase().trim()
-    if(value.length === 0) {
+    if (value.length === 0) {
       alert('NO! NO! NO!')
       this.searchValue = ''
       return
     }
     if (value.length === 1) {
-      this.fetchService.getHeroesByName(value)
+      this.heroByNameSub = this.fetchService.getHeroesByName(value)
         .subscribe(response => {
           this.heroes = response.filter(el => el.name[0].toLowerCase().includes(value.toLowerCase()))
           this.storeService.patchUserState(UserStateKeys.RecentSearch, value)
           this.searchValue = ''
         })
     } else {
-      this.fetchService.getHeroesByName(value)
+      this.heroByNameSub = this.fetchService.getHeroesByName(value)
         .subscribe(response => {
           this.heroes = response.filter(el => el.name.toLowerCase().includes(value.toLowerCase()))
           this.storeService.patchUserState(UserStateKeys.RecentSearch, value)
@@ -54,4 +63,10 @@ export class HeroSelectionPageComponent {
     this.searchValue = el
     this.search()
   }
+
+  ngOnDestroy(): void {
+    this.heroByNameSub.unsubscribe();
+    this.randomHeroSub.unsubscribe()
+  }
+
 }
